c130fd2d8270c3065a0ad149b4f88fefe4ab496f
reallyli <zlireallyli@outlook.com>
2018-12-09 22:58:34 +0800

    init

---
+++ baidu_stat.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?83ca8a6947c3ea070be1452439f08864";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
